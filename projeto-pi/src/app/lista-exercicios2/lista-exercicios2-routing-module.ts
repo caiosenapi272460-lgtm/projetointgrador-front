f2b886b8b755dd5b0b5a2252d41749f32d46c5f3
caiosenapi272460-lgtm <caiosenapi272460@gmail.com>
2026-09-18 01:24:41 +0000

    exercicio 3

--- projeto-pi/src/app/lista-exercicios2/lista-exercicios2-routing-module.ts
+++ projeto-pi/src/app/lista-exercicios2/lista-exercicios2-routing-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Ex1 } from '../ListaExercicios2/ex1/ex1';
 import { Ex2 } from '../ListaExercicios2/ex2/ex2';
+import { Ex3 } from '../ListaExercicios2/ex3/ex3';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: 'ex2', component: Ex2
+  },
+  {
+    path: 'ex3', component: Ex3
   }
 ];
 
