3c3347bc2fb6b26ef449ae225d41020ab3feb024
caiosenapi272460-lgtm <caiosenapi272460@gmail.com>
2026-09-21 19:51:11 +0000

    exercicio 5

---
+++ projeto-pi/src/app/ListaExercicios2/ex5/ex5.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex5',
+  imports: [],
+  templateUrl: './ex5.html',
+  styleUrl: './ex5.scss',
+})
+export class Ex5 {
+  nomes: string[] = [
+    'Caio',
+    'Arthur',
+    'Eric',
+    'Flavio',
+    'Miguel'
+  ];
+}
