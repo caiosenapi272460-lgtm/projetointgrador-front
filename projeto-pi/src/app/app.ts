c16f4be1adfc4ff993fd2a0ffbfdbe672d37c4f7
caiosenapi272460-lgtm <caiosenapi272460@gmail.com>
2026-09-24 23:30:04 +0000

    feat: adiciona componente de cabeçalho e integra ao App

--- projeto-pi/src/app/app.ts
+++ projeto-pi/src/app/app.ts
@@ -1,9 +1,11 @@
+
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './cabecalho/cabecalho';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabecalhoComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
